e59ef3545e03656db612f937a765a329a1a2a9f6
Salvador <salvadoredgar99@gmail.com>
2016-12-04 17:03:48 -0600

    Fist commit

---
+++ app/src/main/java/androidtitlan/gdg/com/processpayments_example/MainActivity.java
@@ -0,0 +1,12 @@
+package androidtitlan.gdg.com.processpayments_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
